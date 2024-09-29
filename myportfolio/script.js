1b5def9941b3b81e14efb13e9ec59788030736bb
Yuireising <yuireisingngalung2000@gmail.com>
2024-09-29 09:05:03 +0530

    Finished Project

---
+++ myportfolio/script.js
@@ -0,0 +1,11 @@
+// sidemenu open and Close
+
+function hidemenu() {
+  const sidemenu = document.querySelector(".sidemenu");
+  sidemenu.style.display = "none";
+}
+
+function showmenu() {
+  const sidemenu = document.querySelector(".sidemenu");
+  sidemenu.style.display = "flex";
+}
